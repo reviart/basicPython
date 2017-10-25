88e3ea356940f3259509b788ff8dcc649908c060
Risjad Revi <reviart18@gmail.com>
2017-10-25 22:32:49 +0700

    no message

---
+++ Numbering.py
@@ -0,0 +1,12 @@
+import sys
+import math
+from math import sqrt
+
+a = math.floor(32.9)
+print(a)
+
+a = int(math.floor(32.9))
+print(a)
+
+a = sqrt(9)
+print("Pangkat = ", a)
---
+++ String.py
@@ -0,0 +1,4 @@
+import sys
+
+name = input("Name? ")
+print("My name is" + name)
