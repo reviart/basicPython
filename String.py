7e7e7217a72a5ec0af93d6d9789bc39d6ac3618a
Risjad Revi <reviart18@gmail.com>
2017-10-28 10:30:24 +0700

    no message

--- String.py
+++ String.py
@@ -1,4 +1,26 @@
 import sys
 
-name = input("Name? ")
-print("My name is" + name)
+#1st input = raw_input
+#name = input("Name? ")
+#print("My name is" + name)
+
+#2nd
+a = "hello, "
+b = "revi"
+print(a + b + "\n")
+
+#3rd
+print \
+    ("testing \n")
+
+a = 1+2+\
+    3+4
+print(a)
+
+#4th raw strings
+print("\n\nRisjad Muhammad \nReviansyah \n")
+
+#5th indexing element
+a = "Revi"
+print(a[1] + a[3])
+print(a[-4]+"\n")
